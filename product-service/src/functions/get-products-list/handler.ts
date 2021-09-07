d8e7bda3b8356766e6128960d1a511d6cfd7e3c1
Net-zen <prankster@myttk.ru>
2021-09-07 21:04:25 +0600

    feat: add pg, integrate db to getProductsList lambda function

--- product-service/src/functions/get-products-list/handler.ts
+++ product-service/src/functions/get-products-list/handler.ts
@@ -6,10 +6,41 @@ import {middyfy} from '@libs/lambda';
 
 import schema from './schema';
 
-import productList from './productList.json';
+import {Client} from 'pg';
+
+const {DB_HOST, DB_PORT, DB_DATABASE, DB_USERNAME, DB_PASSWORD} = process.env;
+const dbOptions = {
+  host: DB_HOST,
+  port: DB_PORT,
+  database: DB_DATABASE,
+  user: DB_USERNAME,
+  password: DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectionTimeoutMillis: 5000
+};
 
 const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async () => {
-  return formatJSONResponse(200, productList);
+  const client = new Client(dbOptions);
+  await client.connect();
+
+  try {
+    const res = await client.query('select p.*, s.count\n' +
+    'from products as p\n' +
+    'left join stocks as s on p.id = s.product_id;'
+  );
+
+    console.log('products from db:', res.rows);
+
+    return formatJSONResponse(200, res.rows);
+  } catch (err) {
+    console.error('Fail to get products from db ', err);
+
+    return formatJSONResponse(500, {message: 'Fail to get products from db'})
+  } finally {
+    await client.end();
+  }
 }
 
 export const main = middyfy(getProductsList);

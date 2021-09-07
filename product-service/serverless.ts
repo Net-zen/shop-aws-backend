83f9fb53ee575eb09682055b10b94b24b2eb89a5
Net-zen <prankster@myttk.ru>
2021-09-07 17:29:33 +0600

    feat: add environment variables for db access in serverless config, add example.env for local environment variables

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -8,6 +8,7 @@ const serverlessConfiguration: AWS = {
     individually: true
   },
   frameworkVersion: '2',
+  useDotenv: true,
   custom: {
     webpack: {
       webpackConfig: './webpack.config.js',
@@ -26,6 +27,11 @@ const serverlessConfiguration: AWS = {
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      DB_HOST: process.env.DB_HOST,
+      DB_PORT: process.env.DB_PORT,
+      DB_DATABASE: process.env.DB_DATABASE,
+      DB_USERNAME: process.env.DB_USERNAME,
+      DB_PASSWORD: process.env.DB_PASSWORD
     },
     lambdaHashingVersion: '20201221',
   },

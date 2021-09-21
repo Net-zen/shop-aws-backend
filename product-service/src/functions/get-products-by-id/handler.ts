42eada08028472d41f2d7fff7b5bb6ce16de94b5
Net-zen <prankster@myttk.ru>
2021-09-21 17:30:25 +0600

    refactor: remove commented code lines

--- product-service/src/functions/get-products-by-id/handler.ts
+++ product-service/src/functions/get-products-by-id/handler.ts
@@ -8,26 +8,6 @@ import schema from './schema';
 
 import { ProductService } from "../../services/product-service"
 
-// import {Client} from 'pg';
-// import {ProductService as realProductService} from "../../services/product-service";
-// import {mockedProductService} from '../../../__tests__/services/mocked-product-service'
-
-// const ProductService = process.env.NODE_ENV === 'test' ? mockedProductService
-//                                                        : realProductService
-
-// const {DB_HOST, DB_PORT, DB_DATABASE, DB_USERNAME, DB_PASSWORD} = process.env;
-// const dbOptions = {
-//   host: DB_HOST,
-//   port: DB_PORT,
-//   database: DB_DATABASE,
-//   user: DB_USERNAME,
-//   password: DB_PASSWORD,
-//   ssl: {
-//     rejectUnauthorized: false
-//   },
-//   connectionTimeoutMillis: 5000
-// };
-
 const getProductsById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   console.log('getProductsById, event: ', event);
 
@@ -41,26 +21,14 @@ const getProductsById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async
 
     return formatJSONResponse(400, {message: `Product id ${id} is not valid`})
   }
-  /* istanbul ignore next */
-  // const client = new Client(dbOptions);
-  // await client.connect();
 
   await ProductService.createClient()
   await ProductService.connect()
 
 
   try {
-    /* istanbul ignore next */
-    // const res = await client.query(`
-    //     select p.*, s.count
-    //     from products as p
-    //     left join stocks as s on p.id = s.product_id
-    //     where p.id = '${id}'`
-    // );
-
     const res = await ProductService.getProduct(id)
 
-    // if (!res.rows[0]) {
     if (!res[0]) {
       console.error(`product with id ${id}:  not found`);
 
@@ -69,17 +37,14 @@ const getProductsById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async
       });
     }
 
-    // console.log(`product with id ${id}: `, res.rows);
     console.log(`product with id ${id}: `, res[0]);
 
-    // return formatJSONResponse(200, res.rows);
     return formatJSONResponse(200, res[0]);
   } catch (err) {
     console.error('Fail to get product from db ', err);
 
     return formatJSONResponse(500, {message: 'Fail to get products from db'})
   } finally {
-    // await client.end();
     await ProductService.disconnect()
   }
 }
--- product-service/src/functions/get-products-list/handler.ts
+++ product-service/src/functions/get-products-list/handler.ts
@@ -8,43 +8,14 @@ import schema from './schema'
 
 import { ProductService } from "../../services/product-service"
 
-// import {Client} from 'pg'
-// import {ProductService as realProductService} from "../../services/product-service"
-// import {mockedProductService} from '../../../__tests__/services/mocked-product-service'
-//
-// const ProductService = process.env.NODE_ENV === 'test' ? mockedProductService
-//                                                        : realProductService
-
-// const {DB_HOST, DB_PORT, DB_DATABASE, DB_USERNAME, DB_PASSWORD} = process.env
-// const dbOptions = {
-//   host: DB_HOST,
-//   port: DB_PORT,
-//   database: DB_DATABASE,
-//   user: DB_USERNAME,
-//   password: DB_PASSWORD,
-//   ssl: {
-//     rejectUnauthorized: false
-//   },
-//   connectionTimeoutMillis: 5000
-// }
 
 const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async () => {
   console.log('getProductsList')
-
-  // const client = new Client(dbOptions)
-  // await client.connect()
 
   await ProductService.createClient()
   await ProductService.connect()
 
   try {
-    /* istanbul ignore next */
-    // const res = await client.query(`
-    //     select p.*, s.count
-    //     from products as p
-    //     left join stocks as s on p.id = s.product_id`
-    // )
-
     const res = await ProductService.getAllProducts()
 
     console.log('products from db:', res)

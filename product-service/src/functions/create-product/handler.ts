747c85e93d9a7598529408d5aa85afbf1e33f69f
Net-zen <prankster@myttk.ru>
2021-10-02 23:57:43 +0600

    feat: extract transaction to db from createProduct to ProductService

--- product-service/src/functions/create-product/handler.ts
+++ product-service/src/functions/create-product/handler.ts
@@ -6,54 +6,28 @@ import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
 
-import { Client } from 'pg';
-
-const {DB_HOST, DB_PORT, DB_DATABASE, DB_USERNAME, DB_PASSWORD} = process.env;
-const dbOptions = {
-  host: DB_HOST,
-  port: DB_PORT,
-  database: DB_DATABASE,
-  user: DB_USERNAME,
-  password: DB_PASSWORD,
-  ssl: {
-    rejectUnauthorized: false
-  },
-  connectionTimeoutMillis: 5000
-};
+import {ProductService} from '../../services/product-service'
 
 const createProduct: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   console.log('createProduct, event: ', event);
 
-  const client = new Client(dbOptions);
-  await client.connect();
-
-  const {title, description, price, image_url, count} = event.body;
+  await ProductService.createClient()
+  await ProductService.connect()
 
   try {
-    await client.query('begin');
-    const res = await client.query(`
-        insert into products (title, description, price, image_url)
-        values ('${title}', '${description}', ${price}, '${image_url}')
-        RETURNING *`
-    );
-    const resCount = await client.query(`
-        insert into stocks (product_id, count)
-        values ((select id from products where id = '${res.rows[0].id}'), ${count})
-        returning count`
-    );
-    await client.query('commit');
 
-    console.log('product, added to db: ', {...res.rows[0], ...resCount.rows[0]});
+    const res = await ProductService.createProduct(event.body)
 
-    return formatJSONResponse(200, {...res.rows[0], ...resCount.rows[0]});
+    console.log('product, added to db: ', {...res});
+
+    return formatJSONResponse(200, {...res});
   } catch (err) {
     console.error('Fail to add product to db ', err);
 
-    await client.query('rollback');
-
     return formatJSONResponse(500, {message: 'Fail to add product to db'})
   } finally {
-    await client.end();
+
+    await ProductService.disconnect()
   }
 }
 

c0374d667d9be7421e3ba88904766f09e238a60e
Net-zen <prankster@myttk.ru>
2021-10-08 13:37:13 +0600

    feat: implement basicAuthorizer lambda function

---
+++ authorization-service/src/functions/basic-authorizer/handler.ts
@@ -0,0 +1,57 @@
+// import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway'
+// import { formatJSONResponse } from '@libs/apiGateway'
+import { middyfy } from '@libs/lambda'
+import { APIGatewayTokenAuthorizerHandler, APIGatewayAuthorizerResult} from 'aws-lambda'
+
+// import schema from './schema'
+
+const basicAuthorizer: APIGatewayTokenAuthorizerHandler = async (event, _ctx, cb) => {
+  console.log('basicAuthorizer, event: ', event)
+
+  if (event['type'] !== 'TOKEN') cb('Unauthorized')
+
+  try {
+    const { authorizationToken } = event
+    const [ _type, encodedCreds ] = authorizationToken
+    const buff = Buffer.from(encodedCreds, 'base64')
+    const [ username, password ] = buff.toString('utf-8').split(':')
+
+    console.log(`Username: ${username}, password: ${password}`)
+
+    const storedUserPassword = process.env[username]
+
+    const effect = !storedUserPassword || storedUserPassword !== password ? 'Deny' : 'Allow'
+
+    const policy = generatePolicy(encodedCreds, event.methodArn, effect)
+
+    cb(null, policy)
+
+    return policy
+  } catch (e) {
+    cb(`Unauthorized: e.message`)
+  }
+
+
+  // return formatJSONResponse({
+  //   message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+  //   event,
+  // });
+}
+
+const generatePolicy = (principalId, Resource, Effect): APIGatewayAuthorizerResult => {
+  return {
+    principalId,
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect,
+          Resource
+        }
+      ]
+    }
+  }
+}
+
+export const main = middyfy(basicAuthorizer);
